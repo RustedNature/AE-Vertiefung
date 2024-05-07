e4e1c040d80fba5ca5cc453b16b3e4b971cb4a4c
RustedNature <133128777+RustedNature@users.noreply.github.com>
2024-05-07 08:58:13 +0200

    Projektdateien hinzufügen.

---
+++ Crash/Fahrzeug.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crash
+{
+    internal class Fahrzeug
+    {
+        private string marke;
+
+        public Fahrzeug(string marke)
+        {
+            Marke = marke;
+        }
+
+        public string Marke { get => marke; set => marke = value; }
+    }
+}
