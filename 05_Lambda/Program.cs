2c8fd61c692da8bf5266293e514505dd1fdf95b8
RustedNature <133128777+RustedNature@users.noreply.github.com>
2024-05-14 14:11:17 +0200

    Delegate und Lambda

---
+++ 05_Lambda/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _05_Lambda
+{
+    internal class Program
+    {
+        //Anonyme Methoden sind nur über delegaten aufrufbar. Für delegeaten => Buch Seite 221
+        //Syntax:
+        //<zugriffsmodifikator> delegate <rückgabetyp> <Delegatennamen>(<parameter>);
+        //
+        //<Delegatennamen> <Bezeichnung> = delegate(<parameter>){methodeninhalt}
+        public delegate void Help();
+
+        private delegate int ErrorCode(int code);
+
+        private delegate string SucheBisLambda(bool da);
+
+        private static void Main(string[] args)
+        {
+            Help help = delegate () { Console.WriteLine("Send Help"); };
+
+            help();
+
+            //Mit lambda =>
+            ErrorCode errorCode = x => x * x;
+
+            Console.WriteLine(errorCode(errorCode(45 * 45)));
+        }
+    }
+}
